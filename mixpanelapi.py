bceea1b51c0a7d5a7e53996218383f3410865c17
jaredmixpanel <jared@mixpanel.com>
2016-07-21 18:40:42 +0200

    Cleanup and refactoring

--- mixpanelapi.py
+++ mixpanelapi.py
@@ -28,17 +28,16 @@ class Mixpanel(object):
     IMPORT_URL = 'https://api.mixpanel.com'
     VERSION = '2.0'
 
-    def __init__(self, api_secret, token=None, timeout=120, pool_size=10, max_retries=10, log_level='warning'):
+    def __init__(self, api_secret, token=None, timeout=120, pool_size=10, max_retries=10, debug=False):
         self.api_secret = api_secret
         self.token = token
         self.timeout = timeout
         self.pool_size = pool_size
         self.max_retries = max_retries
-        if log_level == 'debug':
-            log_level = logging.DEBUG
+        if debug:
+            logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
         else:
-            log_level = logging.WARNING
-        logging.basicConfig(level=log_level)
+            logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.WARNING)
 
     @staticmethod
     def unicode_urlencode(params):
@@ -59,7 +58,7 @@ class Mixpanel(object):
         logging.debug("API Response: " + response)
 
     @staticmethod
-    def write_to_csv(items, output_file):
+    def write_items_to_csv(items, output_file):
         if '$distinct_id' in items[0]:
             props_key = '$properties'
             initial_header_value = '$distinct_id'
@@ -100,7 +99,7 @@ class Mixpanel(object):
             writer.writerow(row)
 
     @staticmethod
-    def properties_from_csv_row(row, header, ignore_columns):
+    def properties_from_csv_row(row, header, ignored_columns):
         props = {}
         for h, prop in enumerate(header):
             # Handle a strange edge case where the length of the row is longer than the length of the header.
@@ -108,13 +107,31 @@ class Mixpanel(object):
             x = h
             if x > len(row) - 1:
                 x = len(row) - 1
-            if row[x] == '' or prop in ignore_columns:
+            if row[x] == '' or prop in ignored_columns:
                 continue
             else:
                 props[prop] = row[x]
         return props
 
-    def _filename_to_list(self, filename):
+    @staticmethod
+    def event_object_from_csv_row(row, header, event_index=None, distinct_id_index=None, time_index=None):
+        event_index = (header.index("event") if event_index is None else event_index)
+        distinct_id_index = (header.index("distinct_id") if distinct_id_index is None else distinct_id_index)
+        time_index = (header.index("time") if time_index is None else time_index)
+        props = {'distinct_id': row[distinct_id_index], 'time': row[time_index]}
+        props.update(Mixpanel.properties_from_csv_row(row, header, ['event', 'distinct_id', 'time']))
+        event = {'event': row[event_index], 'properties': props}
+        return event
+
+    @staticmethod
+    def people_object_from_csv_row(row, header, distinct_id_index=None):
+        distinct_id_index = (header.index("$distinct_id") if distinct_id_index is None else distinct_id_index)
+        props = Mixpanel.properties_from_csv_row(row, header, ['$distinct_id'])
+        profile = {'$distinct_id': row[distinct_id_index], '$properties': props}
+        return profile
+
+    @staticmethod
+    def list_from_items_filename(filename):
         item_list = []
         try:
             with open(filename, 'rbU') as item_file:
@@ -124,51 +141,69 @@ class Mixpanel(object):
                 reader = csv.reader(item_file)
                 header = reader.next()
                 if 'event' in header:
-                    event_name_index = header.index("event")
+                    event_index = header.index("event")
                     distinct_id_index = header.index("distinct_id")
                     time_index = header.index("time")
                     for row in reader:
-                        props = {'token': self.token, 'distinct_id': row[distinct_id_index],
-                                 'time': row[time_index],
-                                 'ip': 0}
-                        props.update(self.properties_from_csv_row(row, header, ['event', 'distinct_id', 'time']))
-                        event = {'event': row[event_name_index], 'properties': props}
+                        event = Mixpanel.event_object_from_csv_row(row, header, event_index, distinct_id_index, time_index)
                         item_list.append(event)
                 elif '$distinct_id' in header:
                     distinct_id_index = header.index("$distinct_id")
                     for row in reader:
-                        props = self.properties_from_csv_row(row, header, ['$distinct_id'])
-                        profile = {'$distinct_id': row[distinct_id_index], '$properties': props}
+                        profile = Mixpanel.people_object_from_csv_row(row, header, distinct_id_index)
                         item_list.append(profile)
         except IOError:
             logging.warning("Error loading data from file: " + filename)
 
         return item_list
 
-    def _get_page(self, params):
-        response = self.request(self.API_URL, ['engage'], params)
+    @staticmethod
+    def _export_data(data, output_file, format='json'):
+        with open(output_file, 'w') as output:
+            if format == 'json':
+                json.dump(data, output)
+            elif format == 'csv':
+                Mixpanel.write_items_to_csv(data, output)
+            else:
+                msg = "Invalid format - must be 'json' or 'csv': format = " + str(format) + '\n' \
+                      + "Dumping json to " + output_file
+                logging.warning(msg)
+                json.dump(data, output)
+
+    @staticmethod
+    def _prep_event_for_import(event, token, timezone_offset):
+        assert ("time" in event['properties']), "Must specify a backdated time"
+        assert ("distinct_id" in event['properties']), "Must specify a distinct ID"
+        event['properties']['time'] = str(
+            int(event['properties']['time']) - (timezone_offset * 3600))  # transforms timestamp to UTC
+        event['properties']["token"] = token
+        return event
+
+    @staticmethod
+    def _prep_profile_for_import(profile, token, ignore_alias):
+        assert token, "Project token required!"
+        params = {
+            '$ignore_time': True,
+            '$ignore_alias': ignore_alias,
+            '$ip': 0,
+            '$token': token,
+            '$distinct_id': profile['$distinct_id'],
+            '$set': profile['$properties']
+        }
+        return params
+
+    def _get_engage_page(self, params):
+        response = self.request(Mixpanel.API_URL, ['engage'], params)
         data = json.loads(response)
         if 'results' in data:
             return data
         else:
             logging.warning("Invalid response from /engage: " + response)
 
-    def _export_data(self, data, output_file, format='json'):
-        with open(output_file, 'w') as output:
-            if format == 'json':
-                json.dump(data, output)
-            elif format == 'csv':
-                self.write_to_csv(data, output)
-            else:
-                msg = "Invalid format - must be 'json' or 'csv': format = " + str(format) + '\n' \
-                    + "Dumping json to " + output_file
-                logging.warning(msg)
-                json.dump(data, output)
-
     def _send_batch(self, endpoint, batch, retries=0):
         payload = {"data": base64.b64encode(json.dumps(batch)), "verbose": 1}
         try:
-            response = self.request(self.IMPORT_URL, [endpoint], payload)
+            response = self.request(Mixpanel.IMPORT_URL, [endpoint], payload)
             msg = "Sent " + str(len(batch)) + " items on " + strftime("%Y-%m-%d %H:%M:%S") + "!"
             logging.debug(msg)
             return response
@@ -186,22 +221,21 @@ class Mixpanel(object):
                 raise
 
     def request(self, base_url, methods, params):
-        if base_url == self.IMPORT_URL:
-            data = self.unicode_urlencode(params)
+        if base_url == Mixpanel.IMPORT_URL:
+            data = Mixpanel.unicode_urlencode(params)
             request_url = '/'.join([base_url] + methods) + '/'
         else:
             data = None
-            request_url = '/'.join([base_url, str(self.VERSION)] + methods) + '/?' + self.unicode_urlencode(params)
+            request_url = '/'.join([base_url, str(Mixpanel.VERSION)] + methods) + '/?' + Mixpanel.unicode_urlencode(params)
         logging.debug("Request URL: " + request_url)
         headers = {'Authorization': 'Basic {encoded_secret}'.format(encoded_secret=base64.b64encode(self.api_secret))}
         request = urllib2.Request(request_url, data, headers)
         response = urllib2.urlopen(request, timeout=self.timeout)
         response_data = response.read()
-        logging.debug("Response: " + response_data)
         return response_data
 
     def query_export(self, params):
-        response = self.request(self.DATA_URL, ['export'], params)
+        response = self.request(Mixpanel.DATA_URL, ['export'], params)
         file_like_object = cStringIO.StringIO(response)
         raw_data = file_like_object.getvalue().split('\n')
         raw_data.pop()
@@ -211,71 +245,57 @@ class Mixpanel(object):
         return events
 
     def query_engage(self, params={}):
-        paginator = ConcurrentPaginator(self._get_page, concurrency=self.pool_size)
+        paginator = ConcurrentPaginator(self._get_engage_page, concurrency=self.pool_size)
         return paginator.fetch_all(params)
 
     def export_events(self, output_file, params, format='json'):
         events = self.query_export(params)
-        self._export_data(events, output_file, format)
+        Mixpanel._export_data(events, output_file, format)
 
     def export_people(self, output_file, params={}, format='json'):
         profiles = self.query_engage(params)
-        self._export_data(profiles, output_file, format)
+        Mixpanel._export_data(profiles, output_file, format)
 
-    def import_events(self, data, timezone_offset=0):
-        event_list = []
+    def _import_data(self, data, item_type, timezone_offset=0, ignore_alias=False):
+        item_list = []
         if isinstance(data, basestring):
-            event_list = self._filename_to_list(data)
+            item_list = Mixpanel.list_from_items_filename(data)
         elif isinstance(data, list):
-            event_list = data
+            item_list = data
         else:
             logging.warning("data parameter must be a filename or a list of events")
 
         pool = ThreadPool(processes=self.pool_size)
         batch = []
-        for event in event_list:
-            assert ("time" in event['properties']), "Must specify a backdated time"
-            assert ("distinct_id" in event['properties']), "Must specify a distinct ID"
-            event['properties']['time'] = str(
-                int(event['properties']['time']) - (timezone_offset * 3600))  # transforms timestamp to UTC
-            if "token" not in event['properties']:
-                assert self.token, "Events must contain a token or one must be supplied on initialization"
-                event['properties']["token"] = self.token
-            batch.append(event)
-            if len(batch) == 50:
-                pool.apply_async(self._send_batch, args=('import', batch), callback=self.response_handler_callback)
-                batch = []
-        if len(batch):
-            pool.apply_async(self._send_batch, args=('import', batch), callback=self.response_handler_callback)
-        pool.close()
-        pool.join()
+        args = [{}, self.token]
 
-    def import_people(self, data):
-        profile_list = []
-        if isinstance(data, basestring):
-            profile_list = self._filename_to_list(data)
-        elif isinstance(data, list):
-            profile_list = data
+        if item_type == 'events':
+            endpoint = 'import'
+            args.append(timezone_offset)
+            prep_function = Mixpanel._prep_event_for_import
+        elif item_type == 'people':
+            endpoint = 'engage'
+            args.append(ignore_alias)
+            prep_function = Mixpanel._prep_profile_for_import
         else:
-            logging.warning("data parameter must be a filename or a list of People profiles")
+            logging.warning('Item type must be "events" or "people", found: ' + str(item_type))
+            return
 
-        pool = ThreadPool(processes=self.pool_size)
-        batch = []
-
-        for profile in profile_list:
-            params = {
-                '$ignore_time': 'true',
-                '$ip': 0,
-                'token': self.token,
-                '$distinct_id': profile['$distinct_id'],
-                '$set': profile['$properties']
-            }
+        for item in item_list:
+            args[0] = item
+            params = prep_function(*args)
             batch.append(params)
             if len(batch) == 50:
-                pool.apply_async(self._send_batch, args=('engage', batch), callback=self.response_handler_callback)
+                pool.apply_async(self._send_batch, args=(endpoint, batch), callback=Mixpanel.response_handler_callback)
                 batch = []
         if len(batch):
-            pool.apply_async(self._send_batch, args=('engage', batch), callback=self.response_handler_callback)
+            pool.apply_async(self._send_batch, args=(endpoint, batch), callback=Mixpanel.response_handler_callback)
         pool.close()
         pool.join()
+
+    def import_events(self, data, timezone_offset=0):
+        self._import_data(data, 'events', timezone_offset=timezone_offset)
+
+    def import_people(self, data, ignore_alias=False):
+        self._import_data(data, 'people', ignore_alias=ignore_alias)
 
